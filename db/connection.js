4e8b5e209db6578ac721ff664b284ef49ebdaadf
Shean <csheanjohnson@gmail.com>
2020-06-18 12:18:23 -0400

    created mongoose connection file

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/chefs-choice-review'
+
+mongoose
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(error => console.error('Connection error', error.message))
+module.exports = mongoose.connection
