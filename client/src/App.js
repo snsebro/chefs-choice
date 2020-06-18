91feab64ad52f7a714ea193686cbb5aadf209e84
Shayne Sebro <shayne@shaynes-mbp.attlocal.net>
2020-06-18 15:47:12 -0400

    react hierarchy

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import Homepage from './pages/homepage/Hompage'
+import ChefPage from './pages/chef-page/ChefPage'
+import ChefProfile from './pages/chef-profile/ChefProfile'
+import SearchPage from './pages/search-page/SearchPage'
+import RecipeDetail from './pages/RecipeDetail/RecipeDetail'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/search' component={SearchPage} />
+        <Route path='/chefs' component={ChefPage}/>
+        <Route path='/chef/:name' component={ChefProfile}/>
+        <Route path='/recipes/:id' component={RecipeDetail}/>
+      </Switch>
     </div>
   );
 }
