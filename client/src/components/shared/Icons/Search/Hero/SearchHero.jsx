a479393d8be099d3a43db71d082d38bfd744eb3f
Shean <csheanjohnson@gmail.com>
2020-06-20 21:22:20 -0400

    linked search component

--- client/src/components/shared/Icons/Search/Hero/SearchHero.jsx
+++ client/src/components/shared/Icons/Search/Hero/SearchHero.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import Search from '../Hero/SearchHero.png'
+import { Link } from 'react-router-dom';
+import Search from '../Hero/SearchHero.png';
 import "../Hero/SearchHero.scss";
 
 export default class Button extends Component {
   render() {
     return (
     <>
-      <img src={Search} />
+        <Link to="/search">
+        <img src={Search} />
+        </Link>
     </>
     );
   }
