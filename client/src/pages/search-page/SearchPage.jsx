6e09b9abbd038a3d8508cee8ab87feca211f6123
Shean <csheanjohnson@gmail.com>
2020-06-20 22:11:36 -0400

    adjusted search functionality to prevent page reload. was being passed as onSubmit, now passed as onChange on line 70 of SearchPage.jsx

--- client/src/pages/search-page/SearchPage.jsx
+++ client/src/pages/search-page/SearchPage.jsx
@@ -67,7 +67,7 @@ class SearchPage extends React.Component {
     return (
       <>
         <h1>Search</h1>
-        <Search onSubmit={this.handleSubmit} value={this.state.filterValue} onChange={this.handleSearchChange} />
+        <Search onChange={this.handleSubmit} value={this.state.filterValue} onChange={this.handleSearchChange} />
         <form className="sort-container" onSubmit={this.handleSubmit}></form>
         <div className="recipes">
            {RECIPES}
