ad4eb0e48504bddfb74baed05b621ff13d704d22
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-06-21 03:13:27 -0400

    css-chef-page

--- client/src/pages/chef-page/ChefPage.jsx
+++ client/src/pages/chef-page/ChefPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import Layout from '../../components/shared/Layout'
 import './ChefPage.scss'
 import data from '../../recipe.json'
@@ -24,16 +24,18 @@ export default class ChefPage extends Component {
       return (
 
         <div className='Chef-List'>
-          <h3>This Week's Top Chefs:</h3>
+          <h2 className='top-chef' >This Week's Top Chefs:</h2>
 
           <div className='general-chef-display'>
             <img className='profile-pic' src={chef.img} />
-            <div className='chef-page-info'>
-              <h4>{chef.chefName}</h4>
-              <Link to={`/chefs/${chef.chefName}`}><button>View Full Profile</button></Link>
+            <div className='chefs-page-info'>
+              <h2>{chef.chefName}</h2>
+              <Link to={`/chefs/${chef.chefName}`}><h1>View Full Profile</h1></Link>
             </div>
           </div>
-          <p>Known for:{chef.knownFor}</p>
+          <p>Known for: {chef.knownFor}</p>
+
+
         </div>
       )
     })
@@ -46,7 +48,7 @@ export default class ChefPage extends Component {
       <>
         <div className='intro-header'>
           <div className='image-header'>
-            <img className='image1'  src='https://images.unsplash.com/photo-1556911220-e15b29be8c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' />
+            <img className='image1' src='https://images.unsplash.com/photo-1556911220-e15b29be8c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' />
             <img className='image2' src='https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' />
           </div>
           <div className='middle-block'>
@@ -59,21 +61,21 @@ export default class ChefPage extends Component {
         </div>
 
         <div className='spotlight'>
-          <h3>Spotlight On:</h3>
+          <h2 className='on'>Spotlight On:</h2>
           <div className='below-spotlight'>
-            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'/>
+            <img src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' className='img-spotlight' />
             <div className='chef-page-info'>
-              <h4>Jennifer Thomas</h4>
-              <img src=''></img>
+              <h2>Jennifer Thomas</h2>
+              <img src='' alt='media-icons' ></img>
             </div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis eget urna risus facilisis sit in fusce.
             </p>
-            <Link to='/chefs/Jennifer Thomas'><button>View Full Profile</button></Link>
+            <Link to='/chefs/Jennifer Thomas'><h1 className='view-profile' >View Full Profile</h1></Link>
           </div>
         </div>
 
-       {allChefs}
+        {allChefs}
       </>
     )
   }
